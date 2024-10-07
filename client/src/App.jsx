31e30aee2420d81e0da8e62aee189490c01f15b9
Rohan Singh <riskysingh897@gmail.com>
2024-10-07 11:00:51 +0530

    install shadcn and the required dependencies

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+
+export default function App() {
+  return (
+    <>
+      <Button className="bg-red-500">Check</Button>
+    </>
+  )
+}
--- src/App.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function App() {
-  return (
-    <div className='text-red-600'>App</div>
-  )
-}
